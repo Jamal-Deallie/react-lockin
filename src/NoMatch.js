735357d795ed2bccc0279f49d6cf01157674c00e
Jamal DeAllie <deallie1985@gmail.com>
2020-10-07 00:07:07 -0400

    third commit

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,11 +1,10 @@
-import React from 'react'
-
+import React from "react";
 
 const NoMatch = () => (
-    <div>
-        <h1>No Match</h1> 
-        <h2>404</h2>
-    </div>
+  <div>
+    <h1>No Match</h1>
+    <h2>404</h2>
+  </div>
 );
 
 export default NoMatch;
